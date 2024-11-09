5b003a50ea791c3198d3ee837a025be4167da11c
Anurag Tomar <anurag.tomar@kapso.in>
2024-11-09 12:08:13 +0000

    testing

--- app/api.py
+++ app/api.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from app.forums.reddit.router import router as reddit_router
-from app.forums.stackoverflow.router import router as stackoverflow_router
-import uvicorn
+
+# from app.forums.reddit.router import router as reddit_router
+# from app.forums.stackoverflow.router import router as stackoverflow_router
+# import uvicorn
 
 app = FastAPI()
 
@@ -11,5 +12,5 @@ def read_root():
     return "Forum ai API"
 
 
-app.include_router(reddit_router, prefix="/api")
-app.include_router(stackoverflow_router, prefix="/api")
+# app.include_router(reddit_router, prefix="/api")
+# app.include_router(stackoverflow_router, prefix="/api")
