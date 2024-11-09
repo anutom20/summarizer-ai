c6a91a43e13e858c3f6c0750a804f287246368ab
Anurag Tomar <anurag.tomar@kapso.in>
2024-11-09 12:12:18 +0000

    removed router

--- app/api.py
+++ app/api.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from fastapi import APIRouter, HTTPException, status
+from app.forums.reddit.reddit_client import reddit_client as reddit
+from loguru import logger
+from app.common import GeminiPrompt
+from app.forums.reddit.utils import extract_comments_from_single_post
+import asyncio
+from app.gemini import generate_summary, get_reddit_summary_prompt_single_post
+from app.config import REDDIT_SEARCH_RESULTS_LIMIT
 
-# from app.forums.reddit.router import router as reddit_router
-# from app.forums.stackoverflow.router import router as stackoverflow_router
-# import uvicorn
 
 app = FastAPI()
 
@@ -12,5 +17,61 @@ def read_root():
     return "Forum ai API"
 
 
-# app.include_router(reddit_router, prefix="/api")
-# app.include_router(stackoverflow_router, prefix="/api")
+@app.get("/get_posts_by_query")
+async def get_posts(question: str):
+    try:
+        all_subreddit = await reddit.subreddit("all")
+
+        posts_info = [
+            dict(post)
+            async for post in all_subreddit.search(
+                question, limit=REDDIT_SEARCH_RESULTS_LIMIT
+            )
+        ]
+
+        logger.debug(f"{posts_info=}")
+
+        return {"posts": posts_info, "total_count": len(posts_info)}
+
+    except Exception as e:
+        logger.error(f"An exception occured while getting posts={e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"{e}"
+        )
+
+
+@app.get("/get_post_comments")
+async def extract_comments(post_url: str):
+    try:
+        comments = await extract_comments_from_single_post(post_url=post_url)
+
+        return comments
+
+    except Exception as e:
+        logger.error(f"exception while fetching comments = {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"{e}"
+        )
+
+
+@app.get("/generate_summary_single_post")
+async def generate_summary_single_post(post_url: str):
+    try:
+        comments = await extract_comments_from_single_post(post_url=post_url)
+
+        prompt_string = get_reddit_summary_prompt_single_post(
+            reddit_comments=comments["comments_output"],
+            post_title=comments["post_title"],
+        )
+
+        gemini_prompt = GeminiPrompt(question=prompt_string)
+
+        llm_res = await generate_summary(gemini_prompt=gemini_prompt)
+
+        return llm_res
+
+    except Exception as e:
+        logger.error(f"exception while generating summary = {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"{e}"
+        )
